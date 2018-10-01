1bf0cd96bf0f7e64292f8d700f323c6e8ae934f6
clunven <cedrick.lunven@gmail.com>
2018-10-01 09:18:13 +0200

    Function

--- aws-function.js
+++ aws-function.js
@@ -1,10 +1,7 @@
-
 'use strict';
-
 const dse           = require('dse-driver');
 const contactPoints = process.env['CONTACT_POINTS'];
 const keyspace      = process.env['KEYSPACE'];
-
 const client        = new dse.Client({
   contactPoints: contactPoints.split(','), keyspace, 
   isMetadataSyncEnabled: false, pooling: { warmup: false }
@@ -20,17 +17,14 @@ client.connect()
   .catch(err => console.error('There was an error trying to connect', err));
 
 /** 
- *Lambda function entry point. 
+ * Lambda function entry point. 
  */
 exports.handler = (event, context, callback) => {
-  
   // Connections are pooled during the lifetime of the Lambda function instance
   context.callbackWaitsForEmptyEventLoop = false;
-
   // Parameters
   const params = [ event.device, event.bucket, 
       new Date(event.date), event.minValue, event.maxValue, event.values ];
-
   client.execute(query, params, { prepare: true })
     .then(() => callback())
     .catch(err => {
